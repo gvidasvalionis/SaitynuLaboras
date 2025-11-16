21f564780326e0dae679f94703cdf510ffe2f297
Gvidas <dievas@DESKTOP-L9G7H79.localdomain>
2025-11-16 23:19:53 +0200

    bomba 123123123

--- backend/src/api.py
+++ backend/src/api.py
@@ -6,3 +6,7 @@ api_router = APIRouter()
 
 api_router.include_router(routers.user_router, prefix="/users", tags=["users"])
 api_router.include_router(routers.auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(routers.driver_router, prefix="/drivers", tags=["drivers"])
+api_router.include_router(routers.team_router, prefix="/teams", tags=["teams"])
+# api_router.include_router(routers.strategy_router, prefix="/strategies", tags=["strategies"])
+api_router.include_router(routers.grand_prix_router, prefix="/grand_prix", tags=["grand_prix"])
--- backend/src/routers/__init__.py
+++ backend/src/routers/__init__.py
@@ -1,2 +1,6 @@
 from src.routers.user import router as user_router
 from src.routers.auth import router as auth_router
+from src.routers.driver import router as driver_router
+from src.routers.team import router as team_router
+# from src.routers.strategy import router as strategy_router
+from src.routers.grand_prix import router as grand_prix_router
